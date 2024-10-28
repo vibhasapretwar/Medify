90eee06d6d3bc876feff96bfb70c78db370f572a
Vibhas Sharad Apretwar <141953399+vibhasapretwar@users.noreply.github.com>
2024-10-28 15:46:41 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {assests} from '../assets/assets';
 import { NavLink } from 'react-router-dom';
-import Navbar from "./Navbar.jsx";
+
 
 const Navbar = () => {
   return (
